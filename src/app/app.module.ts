c07302aa0f8ce0fc7b3f8637a0e3d39bc4516f11
bowei <bowei.han100@gmail.com>
2016-10-02 10:39:04 -0400

    added base components, set up angular structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component/app.component';
+import { AppComponent } from './main/app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+
+import { AllergyClientRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AllergyClientRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,2 +1,2 @@
-export * from './app.component/app.component';
+export * from './main/app.component';
 export * from './app.module';
